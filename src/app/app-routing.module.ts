8780485b917f1989a06d641e6b413dbe0a3b55e6
Abhisheksk234 <abhishekabhi1561@gmail.com>
2024-01-29 15:24:56 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './comp/home/home.component';
+import { AboutusComponent } from './comp/aboutus/aboutus.component';
+import { NavbarComponent } from './comp/navbar/navbar.component';
+import { HeadComponent } from './comp/head/head.component';
+import { ContentComponent } from './comp/content/content.component';
+import { WhyUsSectionComponent } from './comp/why-us-section/why-us-section.component';
+import { SkillsComponent } from './comp/skills/skills.component';
+import { ServiceComponent } from './comp/service/service.component';
+import { CtaComponent } from './comp/cta/cta.component';
+import { PortfolioComponent } from './comp/portfolio/portfolio.component';
+import { TeamComponent } from './comp/team/team.component';
+import { PricingComponent } from './comp/pricing/pricing.component';
+import { QuestionComponent } from './comp/question/question.component';
+import { ContactComponent } from './comp/contact/contact.component';
+import { FooterComponent } from './comp/footer/footer.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  //  { path: 'navbar',component:NavbarComponent },
+  //  { path: 'head', component: HeadComponent },
+  //  { path: 'content', component: ContentComponent },
+  //  { path: 'aboutus', component: AboutusComponent },
+  //  { path: 'why-us-section', component: WhyUsSectionComponent },
+  //  { path: 'skills', component: SkillsComponent},
+  //  { path: 'service', component: ServiceComponent},
+  //  { path: 'cta', component: CtaComponent},
+  //  { path: 'portfolio', component: PortfolioComponent},
+  //  { path: 'team', component: TeamComponent},
+  //  { path: 'pricing', component: PricingComponent},
+  //  { path: 'question', component: QuestionComponent},
+  //  { path: 'contact', component: ContactComponent},
+  //  { path: 'footer', component: FooterComponent},
+  {path:'',component:HomeComponent},
+  {path:'home',component:HomeComponent},
+  {path:'aboutus',component:AboutusComponent},
+  {path:'service',component:ServiceComponent}
+
+   
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,42 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './comp/navbar/navbar.component';
+import { HeadComponent } from './comp/head/head.component';
+import { ContentComponent } from './comp/content/content.component';
+import { FooterComponent } from './comp/footer/footer.component';
+import { HomeComponent } from './comp/home/home.component';
+import { AboutusComponent } from './comp/aboutus/aboutus.component';
+import { WhyUsSectionComponent } from './comp/why-us-section/why-us-section.component';
+import { SkillsComponent } from './comp/skills/skills.component';
+import { ServiceComponent } from './comp/service/service.component';
+import { CtaComponent } from './comp/cta/cta.component';
+import { PortfolioComponent } from './comp/portfolio/portfolio.component';
+import { TeamComponent } from './comp/team/team.component';
+import { PricingComponent } from './comp/pricing/pricing.component';
+import { QuestionComponent } from './comp/question/question.component';
+import { ContactComponent } from './comp/contact/contact.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeadComponent,
+    ContentComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutusComponent,
+    WhyUsSectionComponent,
+    SkillsComponent,
+    ServiceComponent,
+    CtaComponent,
+    PortfolioComponent,
+    TeamComponent,
+    PricingComponent,
+    QuestionComponent,
+    ContactComponent,
+    
   ],
   imports: [
     BrowserModule,
